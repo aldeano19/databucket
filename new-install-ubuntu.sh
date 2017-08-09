42bc72c0025186102a5c15f4775aec8d537c8800
Eriel Marimon <eriel.marimon@sato-global.com>
2017-08-09 13:24:50 -0400

    new set up

---
+++ new-install-ubuntu.sh
@@ -0,0 +1,18 @@
+# INSTALL DOCKER
+sudo apt-get remove docker docker-engine docker.io
+sudo apt-get update
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce
+sudo apt-get install docker-ce=17.06.0~ce-0~ubuntu
+sudo docker run hello-world
+
+# INSTALL DOCKER-COMPOSE
+sudo -i
+curl -L https://github.com/docker/compose/releases/download/1.15.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+docker-compose --version
