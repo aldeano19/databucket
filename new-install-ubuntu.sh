60714943b0c792aafbe26b6ff1cc6bf9d9f48d86
Eriel Marimon <eriel.marimon@sato-global.com>
2017-08-16 23:11:59 -0400

    ns

--- new-install-ubuntu.sh
+++ new-install-ubuntu.sh
@@ -1,4 +1,9 @@
 # INSTALL DOCKER
+
+echo "-----------"
+echo "ECHO #1"
+echo "-----------"
+
 sudo apt-get remove docker docker-engine docker.io
 sudo apt-get update
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
@@ -6,10 +11,21 @@ sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
+
+echo "-----------"
+echo "ECHO #2"
+echo "-----------"
+
 sudo apt-get update
+
+# TODO: This line will not install on itself, needs to be run manually.
 sudo apt-get install docker-ce
 sudo apt-get install docker-ce=17.06.0~ce-0~ubuntu
 sudo docker run hello-world
+
+echo "-----------"
+echo "ECHO #3"
+echo "-----------"
 
 # INSTALL DOCKER-COMPOSE
 sudo -i
